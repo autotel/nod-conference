4f8315d0516c35ab920a79a7020e59d7d7a46ae5
Joaquín <joaquin.aldunate@gmail.com>
2018-07-09 12:52:51 +0300

    include settings

--- settings.js
+++ settings.js
@@ -1,10 +1,16 @@
 var settings={
+  electronClient:{
+    socketServer:'http://192.168.11.3',
+    socketPort:8081,
+  },
   clientServer:{
     use:true,
-    port:8080,
+    porthttps: 8080,
+    porthttp: 8081,
+    port:8081,
     keyFile:'./server/sslcert/server.key',
     certificateFile:'./server/sslcert/server.crt',
-    verbose:false
+    verbose:true
   }
 }
 module.exports=settings;
